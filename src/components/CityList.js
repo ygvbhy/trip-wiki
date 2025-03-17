37444d4b7b18c0956211593e5ccf130884f5e96c
ygvbhy <ygvbhy@naver.com>
2025-03-17 21:02:59 +0900

    메인 여행지 리스트 출력

--- src/components/CityList.js
+++ src/components/CityList.js
@@ -1,10 +1,43 @@
-export default function CityList() {
+export default function CityList({ $app, initialState, handleLoadMore }) {
+  this.state = initialState;
   this.$target = document.createElement("div");
   this.$target.className = "city-list";
+  this.handleLoadMore = handleLoadMore;
 
-  this.template = () => {};
+  $app.appendChild(this.$target);
 
-  this.render = () => {};
+  this.template = () => {
+    let temp = `<div class="city-items-container">`;
+
+    console.log(this.state);
+
+    if (this.state) {
+      this.state.cities.forEach((city) => {
+        temp += `
+        <div class="city-item" id="${city.id}">
+          <img src="${city.image}" />
+          <div class="city-item-info">${city.city} ${city.country}</div>
+          <div class="city-item-score">⭐️ ${city.total}</div>
+        </div>`;
+      });
+      temp += `</div>`;
+    }
+    return temp;
+  };
+
+  this.render = () => {
+    this.$target.innerHTML = this.template();
+    if (!this.state.isEnd) {
+      const $loadMoreButton = document.createElement("button");
+      $loadMoreButton.className = "add-items-btn";
+      $loadMoreButton.textContent = "+ 더보기";
+      this.$target.appendChild($loadMoreButton);
+
+      $loadMoreButton.addEventListener("click", () => {
+        this.handleLoadMore();
+      });
+    }
+  };
 
   this.setState = (newState) => {
     this.state = newState;
---
+++ src/components/api.js
@@ -0,0 +1,27 @@
+const API_URL = "https://trip-wiki-api.vercel.app/";
+
+export const request = async (startIdx, region, sortBy, searchWord) => {
+  try {
+    let url = `${API_URL}`;
+    if (region && region !== "All") {
+      url += `${region}?start=${startIdx}`;
+    } else {
+      url += `?start=${startIdx}`;
+    }
+
+    if (sortBy) {
+      url += `&sort=${sortBy}`;
+    }
+    if (searchWord) {
+      url += `&search=${searchWord}`;
+    }
+
+    const response = await fetch(url);
+    if (response) {
+      const data = await response.json();
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
